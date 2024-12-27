c8a29532c52c8c1a70c5679b13f124bc7e684ed4
Mikhail Sannikov <100658033+msannikov03@users.noreply.github.com>
2024-12-27 17:03:35 +0300

    Production build 2.0.

--- src/app/confirmation/page.tsx
+++ src/app/confirmation/page.tsx
@@ -1,12 +1,20 @@
-'use client';
+"use client";
 
-import { motion } from 'framer-motion';
-import Link from 'next/link';
-import { CheckCircle } from 'lucide-react';
-import { Button } from '@/components/ui/button';
-import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
+import { motion } from "framer-motion";
+import Link from "next/link";
+import { CheckCircle } from "lucide-react";
+import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { useCart } from "../../contexts/CartContext";
+import { useEffect } from "react";
 
 export default function OrderConfirmation() {
+  const { clearCart } = useCart();
+
+  useEffect(() => {
+    clearCart();
+  }, []);
+
   return (
     <div className="bg-gray-50 flex items-center justify-center">
       <Card className="w-full max-w-lg">
@@ -14,7 +22,7 @@ export default function OrderConfirmation() {
           <motion.div
             initial={{ scale: 0 }}
             animate={{ scale: 1 }}
-            transition={{ type: 'spring', stiffness: 260, damping: 20 }}
+            transition={{ type: "spring", stiffness: 260, damping: 20 }}
             className="flex justify-center"
           >
             <CheckCircle className="w-16 h-16 text-green-500" />
@@ -23,7 +31,9 @@ export default function OrderConfirmation() {
         </CardHeader>
         <CardContent className="text-center space-y-4">
           <p>Спасибо за покупку. Ваш заказ был успешно оформлен.</p>
-          <p>Детали вашего заказа были направлены на адрес электронной почты.</p>
+          <p>
+            Детали вашего заказа были направлены на адрес электронной почты.
+          </p>
           <Button asChild>
             <Link href="/">На главную</Link>
           </Button>
